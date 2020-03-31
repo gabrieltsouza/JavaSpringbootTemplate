a0172f04d62f8cd14bd0d034f248c02adb86f287
gabrieltsouza <gtsouza@mandic.com.br>
2020-03-31 14:37:59 -0300

    Subindo os arquivos do projeto template

---
+++ src/main/java/br/com/ecommerce/fhvgts/FhvgtsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.fhvgts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhvgtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhvgtsApplication.class, args);
+	}
+
+}
